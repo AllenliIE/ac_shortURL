f2c56d7eac28862b1756de0cb1ce4eda868db963
AllenliIE <allen.li.service@gmail.com>
2022-07-17 11:36:35 +0800

    feat: add change view for shortURL

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const { Error } = require('mongoose')
+const URL = require('./models/URL')
+
+
 
 //Setting Mongoose
 const mongoose = require('mongoose')
@@ -27,6 +31,25 @@ app.set('view engine', 'hbs')  //open handlebars
 //Setting index.js router
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+//Setting shortURL Change
+app.post('/', (req, res) => {
+  //當index的input不是url時，回到首頁
+  if (!req.body.url) return res.redirect('/')
+  const originalURL = req.body.url
+
+  URL.findOne({ originalURL })
+    //如果data正確，回傳data，否則URL新增短網址與原始網址
+    .then(data => data ? data : URL.create({ shortURL, originalURL }))
+    .then(data =>
+      //回到index頁面顯示原始網址與短網址
+      res.render('index', {
+        origin: req.headers.origin,
+        shortURL: data.shortURL
+      })
+    )
+    .catch(error => console.log(error))
 })
 
 //Setting port 3000
